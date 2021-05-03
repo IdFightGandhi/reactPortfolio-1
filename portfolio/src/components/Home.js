bae49b13ce600363c67df063398a88f88e54b253
William Thorne <williamcthorne@gmail.com>
2021-05-02 17:18:33 -0700

    Home Page start

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import image from "../img/background.jpg"
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="Background" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className>It's me, Will!</h1>
+            </section>
+        </main>
+    )
 }
