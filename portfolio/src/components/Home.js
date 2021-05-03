9b39c746a700f82c326e0c9ec9836140c85fcef3
William Thorne <williamcthorne@gmail.com>
2021-05-02 20:57:36 -0700

    start blog post page, cards don't show yet

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <img src={image} alt="Background" className="absolute object-cover w-full h-full" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className>It's me, Will!</h1>
+                <h1 className="text-6xl text-green-100 fond-bold cursive leading-none lg:leading-snug home-name">It's me, Will!</h1>
             </section>
         </main>
     )
